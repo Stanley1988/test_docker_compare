e3bb96ecdfc6d15160200fda383dc9d1a10d1364
Stanley Mtonyole <stanleymtonyole@gmail.com>
2024-04-19 10:49:37 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Import the Express module
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a route handler for the root path
+app.get('/', (req, res) => {
+  res.send('Backend Numerical');
+});
+
+app.get('/compare/:value', (req, res) => {
+  let baseNum = 50;
+  let respSMS = req.params.value+' is less than 50';
+  if(req.params.value > baseNum){
+    respSMS = req.params.value+' is greater than 50';
+  }
+    res.send(respSMS);
+});
+
+// Start the server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
